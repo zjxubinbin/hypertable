0f4bc55a88cce98ed1f3d4a22a1082fc75da92fc
Christoph Rupp <chris@crupp.de>
2012-01-16 16:41:22 +0100

    Issue #760: Updated LICENSE.txt and sources to GPL v3

--- src/cc/Hypertable/RangeServer/CellStoreV4.h
+++ src/cc/Hypertable/RangeServer/CellStoreV4.h
@@ -5,7 +5,7 @@
  *
  * Hypertable is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; version 2 of the
+ * as published by the Free Software Foundation; version 3 of the
  * License.
  *
  * Hypertable is distributed in the hope that it will be useful,
